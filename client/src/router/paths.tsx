65116dfcc65dc5fcb92f96a64324fbe44b4ed113
AdiMalkiel <adimalkiel2@gmail.com>
2025-11-26 13:20:42 +0200

    feat(/client): Added init files and component of single product

---
+++ client/src/router/paths.tsx
@@ -0,0 +1,44 @@
+// import { EditConvoy } from "../views/EditConvoy";
+// import { Home } from "../views/Home";
+// import { Map } from "../views/Map";
+
+// import Layout from "../views/Layout";
+
+// export interface Page {
+//   path: string;
+//   element: JSX.Element;
+//   name: string;
+//   isShown: boolean;
+// }
+
+// export const routes: Page[] = [
+//   {
+//     path: "/",
+//     element: <Home />,
+//     name: "בית",
+//     isShown: true,
+//   },
+//   {
+//     path: "/map",
+//     element: <Map />,
+//     name: "מפה",
+//     isShown: true,
+//   },
+//   {
+//     path: "/edit",
+//     element: <EditConvoy />,
+//     name: "Edit",
+//     isShown: false,
+//   },
+// ];
+
+// export const paths = [
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: routes.map((route) => ({
+//       path: route.path,
+//       element: route.element,
+//     })),
+//   },
+// ];
