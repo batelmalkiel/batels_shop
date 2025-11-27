cbfe6b6a46af21fda9ebb48b360701e0402290f7
AdiMalkiel <adimalkiel2@gmail.com>
2025-11-27 09:29:05 +0200

    feat(paths.tsx, layout.tsx): Added basic paths and layout

--- client/src/router/paths.tsx
+++ client/src/router/paths.tsx
@@ -1,44 +1,30 @@
-// import { EditConvoy } from "../views/EditConvoy";
-// import { Home } from "../views/Home";
-// import { Map } from "../views/Map";
+import { Home } from "../views/Home";
 
-// import Layout from "../views/Layout";
+import Layout from "../views/Layout";
 
-// export interface Page {
-//   path: string;
-//   element: JSX.Element;
-//   name: string;
-//   isShown: boolean;
-// }
+export interface Page {
+  path: string;
+  element: JSX.Element;
+  name: string;
+  isShown: boolean;
+}
 
-// export const routes: Page[] = [
-//   {
-//     path: "/",
-//     element: <Home />,
-//     name: "בית",
-//     isShown: true,
-//   },
-//   {
-//     path: "/map",
-//     element: <Map />,
-//     name: "מפה",
-//     isShown: true,
-//   },
-//   {
-//     path: "/edit",
-//     element: <EditConvoy />,
-//     name: "Edit",
-//     isShown: false,
-//   },
-// ];
+export const routes: Page[] = [
+  {
+    path: "/",
+    element: <Home />,
+    name: "בית",
+    isShown: true,
+  },
+];
 
-// export const paths = [
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: routes.map((route) => ({
-//       path: route.path,
-//       element: route.element,
-//     })),
-//   },
-// ];
+export const paths = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: routes.map((route) => ({
+      path: route.path,
+      element: route.element,
+    })),
+  },
+];
